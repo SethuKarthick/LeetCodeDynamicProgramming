1b7cb1e45cb0e417cd8698d90ae1e6f5c81f594d
SethuKarthick <51673841+SethuKarthick@users.noreply.github.com>
2023-09-05 09:51:14 +0530

    Time: 2 ms (32.35%), Space: 2.4 MB (8.82%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.go
@@ -0,0 +1,24 @@
+func diffWaysToCompute(input string) []int {
+    ans := []int{}
+
+    for i:=0; i< len(input); i++{
+        if input[i] == '+' || input[i] == '-' || input[i] == '*'{
+            for _, a := range diffWaysToCompute(input[:i]){
+                for _, b := range diffWaysToCompute(input[i+1:]){
+                    switch input[i]{
+                        case '+':
+                            ans = append(ans, a+b)
+                        case '-' :
+                            ans = append(ans, a-b)
+                        case '*' :
+                            ans = append(ans, a*b)
+                    }
+                }
+            }
+        }
+    }
+    if temp, _ := strconv.Atoi(input); len(ans) == 0{
+        ans = append(ans, temp)
+    }
+    return ans
+}
