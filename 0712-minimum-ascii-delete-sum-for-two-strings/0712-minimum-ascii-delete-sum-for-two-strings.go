fdf2cfda152c5155fd4559f8f3c77298f6dec29a
SethuKarthick <51673841+SethuKarthick@users.noreply.github.com>
2023-09-08 19:28:17 +0530

    Time: 797 ms (5.14%), Space: 27 MB (5.55%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.go
@@ -0,0 +1,49 @@
+func minimumDeleteSum(s1 string, s2 string) int {
+    var (
+        solve  func(i, j int) int
+        getMinSum func(str string, i int) int
+    )
+
+    mp := make(map[string]int)
+    m, n := len(s1), len(s2)
+
+
+    getMinSum = func(str string, i int) int {
+        sum := 0
+        for ; i< len(str); i++{
+            sum += int(str[i])
+        }
+        return sum 
+    }
+
+    solve = func(i, j int) int {
+        sum := 0
+        if i==m && j==n {
+            return 0
+        }
+        if i==m {
+            return getMinSum(s2, j)
+        }else if j == n {
+            return getMinSum(s1, i)
+        }
+        key := fmt.Sprintf("%d-%d", i, j)
+        if v, ok :=  mp[key]; ok {
+            return v
+        }
+        if s1[i] == s2[j]{
+            sum = solve(i+1, j+1)
+        }else {
+            sum = min(solve(i+1, j)+int(s1[i]), solve(i, j+1)+int(s2[j]))
+        }
+        mp[key] = sum 
+        return mp[key]
+    }
+    return solve(0, 0)
+}
+
+func min(a, b int) int{
+    if a < b {
+        return a 
+    }
+    return b
+}
