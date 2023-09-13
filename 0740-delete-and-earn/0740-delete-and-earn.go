b953a62234314b9789e718add41102c32140578b
SethuKarthick <51673841+SethuKarthick@users.noreply.github.com>
2023-09-13 07:55:01 -0700

    Time: 9 ms (58.24%), Space: 5.1 MB (75.27%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.go
+++ 0740-delete-and-earn/0740-delete-and-earn.go
@@ -1,12 +1,23 @@
 func deleteAndEarn(nums []int) int {
-    dp := make([]int, 10003)
+    l :=  10002
+    dp := make([]int, 10002)
     for _, v := range nums{
         dp[v] += v
     }
-    for i:=3; i<=10001; i++{
-        dp[i] = max(dp[i-1], max(dp[i-2], dp[i-3])+ dp[i])
+
+    a := dp[l-2]
+    b := 0
+
+    for i:=l-3; i>=0; i--{
+        c := max(dp[i]+ b, a)
+        b = a
+        a = c
+        // dp[i] = max(dp[i-1], max(dp[i-2], dp[i-3])+ dp[i])
     }
-    return dp[10001]
+    // return dp[10001]
+    return a
+
+    
 }
 
 func max(a, b int) int{
