f42d91c06d109b97b90807b7e0db84eaf5626134
SethuKarthick <51673841+SethuKarthick@users.noreply.github.com>
2025-01-09 12:00:22 +0530

    3309-COUNT-PREFIX-AND-SUFFIX-PAIRS-I Stats: Time: 11 ms (56.17%), Space: 17.6 MB (36.75%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.py
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.py
@@ -3,17 +3,23 @@ class Solution:
         count = 0
         n = len(words)
 
-        for i in range(n):
-            for j in range(i+1, n):
-                if self.isPrefixAndSuffixPairs(words[i], words[j]):
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         if self.isPrefixAndSuffixPairs(words[i], words[j]):
+        #             count += 1
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+            # Check if words[i] is both a prefix AND a suffix of words[j]
+                if words[j].startswith(words[i]) and words[j].endswith(words[i]):
+                # Yo, we found a match! Increment the count.
                     count += 1
         return count
 
-    def isPrefixAndSuffixPairs(self, str1, str2):
-        n1 = len(str1)
-        n2 = len(str2)
+    # def isPrefixAndSuffixPairs(self, str1, str2):
+    #     n1 = len(str1)
+    #     n2 = len(str2)
 
-        if n1 > n2:
-            return False
+    #     if n1 > n2:
+    #         return False
 
         return str2[:n1] == str1 and str2[-n1:] == str1
