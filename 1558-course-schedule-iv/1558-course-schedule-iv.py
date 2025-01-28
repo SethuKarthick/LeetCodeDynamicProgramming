b7bb2dab13c1e03f15faecc7e46efd65734bf5b7
SethuKarthick <51673841+SethuKarthick@users.noreply.github.com>
2025-01-28 10:52:59 +0530

    1558-COURSE-SCHEDULE-IV Stats: Time: 351 ms (31.57%), Space: 23.1 MB (5.13%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.py
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -1,5 +1,26 @@
+from collections import defaultdict
+from typing import List
+
 class Solution:
-    def checkIfPrerequisite(self, _: int, p: List[List[int]], q: List[List[int]]) -> List[bool]:
-        g = {v:{u for _,u in w} for v,w in groupby(p,itemgetter(0))}
-        f = cache(lambda v,u:u in g.get(v,[]) or any(f(i,u) for i in g.get(v,[])))
-        return [*starmap(f, q)]
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        prereq_map = defaultdict(list)  # course -> prereqs
+
+        for prereq in prerequisites:
+            prereq_map[prereq[1]].append(prereq[0])
+
+        res = []
+        memo = {}
+        def dfs(course, prereq):
+            if (course, prereq) in memo:
+                return memo[(course, prereq)]
+            
+            course_prereqs = prereq_map[course]
+            for p in course_prereqs:
+                if p == prereq or dfs(p, prereq):
+                    memo[(course, prereq)] = True
+                    return True
+            
+            memo[(course, prereq)] = False
+            return False
+        
+        return [dfs(query[1], query[0]) for query in queries]
