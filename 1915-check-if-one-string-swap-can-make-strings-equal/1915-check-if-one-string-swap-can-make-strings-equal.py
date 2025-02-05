f49d462921465811fbc289b2803c020a81ac4c59
SethuKarthick <51673841+SethuKarthick@users.noreply.github.com>
2025-02-05 06:54:19 +0400

    1915-CHECK-IF-ONE-STRING-SWAP-CAN-MAKE-STRINGS-EQUAL Stats: Time: 0 ms (100%), Space: 17.6 MB (67.41%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
@@ -18,4 +18,13 @@ class Solution:
         i, j = diff 
 
         return s1[i] == s2[j] and s1[j] == s2[i]
+        # if len(s1) != len(s2):
+        #     return False
+
+        # total = 0
+        # for i, j in zip(s1, s2):
+        #     if i != j:
+        #         total += 1
+            
+        # return total == 0 or total == 2
 
