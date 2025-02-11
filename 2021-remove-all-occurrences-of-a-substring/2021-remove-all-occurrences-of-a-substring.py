5577800d1022a47de1c43a462eb2e62cef5518d0
SethuKarthick <51673841+SethuKarthick@users.noreply.github.com>
2025-02-11 15:17:35 +0400

    2021-REMOVE-ALL-OCCURRENCES-OF-A-SUBSTRING Stats: Time: 0 ms (100%), Space: 17.9 MB (32.79%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.py
@@ -1,13 +1,16 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        result_stack = []
-        target_length = len(part)
-        target_end_char = part[-1]
+        # result_stack = []
+        # target_length = len(part)
+        # target_end_char = part[-1]
 
-        for currentChar in s:
-            result_stack.append(currentChar)
-            if currentChar == target_end_char and len(result_stack) >= target_length:
-                if "".join(result_stack[-target_length:]) == part:
-                    del result_stack[-target_length:]
+        # for currentChar in s:
+        #     result_stack.append(currentChar)
+        #     if currentChar == target_end_char and len(result_stack) >= target_length:
+        #         if "".join(result_stack[-target_length:]) == part:
+        #             del result_stack[-target_length:]
                 
-        return "".join(result_stack)
+        # return "".join(result_stack)
+        while part in s:
+            s = s.replace(part,"",1)
+        return s
