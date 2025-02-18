6b9a4aea2074367cf1625dd7d43d5a627fb38dba
SethuKarthick <51673841+SethuKarthick@users.noreply.github.com>
2025-02-18 07:59:47 +0530

    2456-CONSTRUCT-SMALLEST-NUMBER-FROM-DI-STRING Stats: Time: 29 ms (18.22%), Space: 17.8 MB (54.04%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.py
@@ -1,19 +1,32 @@
 class Solution:
+    # def smallestNumber(self, pattern: str) -> str:
+    #     n=len(pattern)
+    #     d=[0]*n
+    #     d[-1]+=(pattern[-1]=='D')
+    #     for i in range(n-2, -1, -1):
+    #         d[i]=d[i+1]+1 if pattern[i]=='D' else 0
+    #     ans=[ chr(i+ord('1')) for i in range(n+1)]
+    #     i=0
+    #     while i<n:
+    #         if pattern[i]=='D':
+    #             l, r=i, i+d[i]
+    #             while l<r:
+    #                 ans[l], ans[r]=ans[r], ans[l]
+    #                 l+=1
+    #                 r-=1
+    #         i+=d[i]+1
+    #     return "".join(ans)
     def smallestNumber(self, pattern: str) -> str:
         n=len(pattern)
-        d=[0]*n
-        d[-1]+=(pattern[-1]=='D')
-        for i in range(n-2, -1, -1):
-            d[i]=d[i+1]+1 if pattern[i]=='D' else 0
-        ans=[ chr(i+ord('1')) for i in range(n+1)]
+        res=[i for i in range(1,n+2)]
         i=0
         while i<n:
-            if pattern[i]=='D':
-                l, r=i, i+d[i]
-                while l<r:
-                    ans[l], ans[r]=ans[r], ans[l]
-                    l+=1
-                    r-=1
-            i+=d[i]+1
-        return "".join(ans)
+            t=i
+            while t< n and pattern[t]=="D":
+                t+=1
+            res[i:t+1] =reversed(res[i:t+1])
+            if t!=i:
+                i=t-1
+            i+=1
+        return "".join(map(str,res))
         
