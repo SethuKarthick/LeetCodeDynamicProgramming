f49f55886761ece389d752181b93fe8dc155091e
SethuKarthick <51673841+SethuKarthick@users.noreply.github.com>
2025-07-02 12:52:06 +0530

    0875-LONGEST-MOUNTAIN-IN-ARRAY Stats: Time: 9 ms (82.25%), Space: 18.8 MB (33.55%) - LeetHub

--- 0875-longest-mountain-in-array/0875-longest-mountain-in-array.py
+++ 0875-longest-mountain-in-array/0875-longest-mountain-in-array.py
@@ -1,23 +1,43 @@
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
-        longestPeak = 0
+        # longestPeak = 0
         
-        for i in range(1, len(arr)-1):
-            isPeak = arr[i-1] < arr[i] > arr[i+1]
+        # for i in range(1, len(arr)-1):
+        #     isPeak = arr[i-1] < arr[i] > arr[i+1]
+        #     if not isPeak:
+        #         continue
+
+        #     left = i - 1
+        #     while left > 0 and arr[left] >  arr[left - 1]:
+        #         left -= 1
+
+        #     right = i + 1
+            
+        #     while right < len(arr) - 1 and arr[right] > arr[right+1]:
+        #         right += 1
+
+        #     currentPeak = right - left + 1
+        #     if currentPeak > longestPeak:
+        #         longestPeak = currentPeak
+
+        # return longestPeak
+        longestPeak = 0
+
+        n = len(arr) - 1
+
+        for i in range(1, n):
+            isPeak =  arr[i-1] < arr[i] > arr[i+1]
             if not isPeak:
                 continue
 
-            left = i - 1
-            while left > 0 and arr[left] >  arr[left - 1]:
-                left -= 1
+            left = right = i
 
-            right = i + 1
+            while left > 0 and arr[left] > arr[left-1]:
+                left -= 1
             
-            while right < len(arr) - 1 and arr[right] > arr[right+1]:
+            while right < n and arr[right] > arr[right+1]:
                 right += 1
 
-            currentPeak = right - left + 1
-            if currentPeak > longestPeak:
-                longestPeak = currentPeak
+            longestPeak = max(longestPeak, right - left + 1)
 
         return longestPeak
